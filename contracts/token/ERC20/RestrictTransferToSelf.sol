8aaeb3f68edd55efd1dc6c9a5eab4634d35c54dc
Jitendra Chittoda <jcchittoda@gmail.com>
2018-03-01 05:47:55 +0530

    Restrict sending own ERC20 tokens to self contract. Contract will reject tokens sent to his own address.

---
+++ contracts/token/ERC20/RestrictTransferToSelf.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.18;
+
+import "./StandardToken.sol";
+
+/**
+ * @title Restrict sending ERC20 tokens to self
+ * @dev This contract will restrict users to wrongly send tokens to self Token Contract.
+ */
+contract RestrictTransferToSelf is StandardToken {
+    
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(this));
+
+        return super.transfer(_to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(this));
+
+        return super.transferFrom(_from, _to, _value);
+    }
+}
